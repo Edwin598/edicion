b3abe727613e164cdcf3b01613671f287f8de8f2
edwin <edwin>
2022-02-11 22:56:08 +0000

    se agrego la funcion para asignar valores nulos.

--- edicion.c
+++ edicion.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <fcntl.h>
+int asignacionNull(char *[]);
+int wrOut(char *[], int *);
 int main(void){
 	int count1,count2,cBytes=0,comparadorBytes;
 	char bufferOut[1000], bufferInt[cBytes];
+	asignacionNull();
+};
+int asignacionNull(char *array[]){
+	for(int i=0;i<=1000;i++){
+		array[i]='\0';
+	};
+};
+int wrOut(char *array1, int count){
+
 };
